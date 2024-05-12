036f11216d37248d5d87de0c1c74aecae8024443
Daniel Hall <daniel@danielhall.me>
2024-05-12 12:13:17 +1000

    Publish a message upon webhook

--- internal/publisher/publisher.go
+++ internal/publisher/publisher.go
@@ -1,15 +1,64 @@
 package publisher
 
-import "net/http"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
 
-type Publisher struct{}
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Publisher struct {
+	conn *amqp.Connection
+	ch   *amqp.Channel
+}
 
 func New() *Publisher {
-	return &Publisher{}
+	var err error
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Panicf("Failed to connect to RabbitMQ: %s", err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Panicf("Failed to open a channel: %s", err)
+	}
+
+	err = ch.ExchangeDeclare(
+		"webhooks", // name
+		"topic",    // type
+		true,       // durable
+		false,      // auto-deleted
+		false,      // internal
+		false,      // no-wait
+		nil,        // arguments
+	)
+	if err != nil {
+		log.Panicf("Failed to declare exchange: %s", err)
+	}
+
+	return &Publisher{
+		conn: conn,
+		ch:   ch,
+	}
 }
 
 func (p *Publisher) Publish(request http.Request) error {
-	// Publish the message
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
-	return nil
+	err := p.ch.PublishWithContext(ctx,
+		"webhooks", // exchange
+		"test",     // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World!"),
+		})
+
+	return err
 }
